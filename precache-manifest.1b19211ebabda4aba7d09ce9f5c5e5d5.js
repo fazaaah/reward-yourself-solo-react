d2a478fbc8afa89e8c37db6b539c4d00bcb0dc83
fazaa <fazaa.ahmed93@gmail.com>
2019-06-03 13:30:00 -0400

    Updates

---
+++ precache-manifest.1b19211ebabda4aba7d09ce9f5c5e5d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5234f192ef4fd7b03dc0d901cca634eb",
+    "url": "/project-5-solo-react/index.html"
+  },
+  {
+    "revision": "138e3e4b133f2cc65cf5",
+    "url": "/project-5-solo-react/static/css/main.ab10c862.chunk.css"
+  },
+  {
+    "revision": "027807b6c6eb4ce166d2",
+    "url": "/project-5-solo-react/static/js/2.b3f50240.chunk.js"
+  },
+  {
+    "revision": "138e3e4b133f2cc65cf5",
+    "url": "/project-5-solo-react/static/js/main.3051c883.chunk.js"
+  },
+  {
+    "revision": "346a916821ba10bf04fa",
+    "url": "/project-5-solo-react/static/js/runtime~main.37216aff.js"
+  },
+  {
+    "revision": "b480a3e35378656ff2be8ac66aeb6a2f",
+    "url": "/project-5-solo-react/static/media/background.b480a3e3.png"
+  }
+]);
